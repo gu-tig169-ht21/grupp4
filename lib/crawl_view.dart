a6147b6cb83f1acab8ec2e5ee38effe3f39af82a
Stefan Lelouda <stefanchromebook@gmail.com>
2021-12-15 09:16:47 +0100

    tog bort ena crawlist och småändringar

--- lib/crawl_view.dart
+++ lib/crawl_view.dart
@@ -15,22 +15,6 @@ class CrawlView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('crawl view'),
-            ElevatedButton(
-              child: Text('(till login'),
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => LoginScreen())),
-            ),
-            ElevatedButton(
-              child: Text('(till en specifik crawl)'),
-              onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => CrawlDetails())),
-            ),
-            ElevatedButton(
-              child: Text('Till dina favoriter'),
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => Favorites())),
-            ),
             ElevatedButton(
               child: Text('Till start screen'),
               onPressed: () => Navigator.of(context)
@@ -41,4 +25,32 @@ class CrawlView extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget buildCard() {
+  return Center(
+    child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(), //bilden
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 16, top: 16, right: 16, bottom: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Do or die'),
+                ],
+              ),
+            ), //texten under
+            ButtonBar(), //knapparna
+          ],
+        ),
+      ),
+    ),
+  );
 }
